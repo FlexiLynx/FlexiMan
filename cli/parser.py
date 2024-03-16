e2caf5be94a03298df35c90083b40395c5b963c0
Shae <a.thomas.vc@gmail.com>
2024-03-16 15:55:33 -0700

    Removed `click`

--- cli/parser.py
+++ cli/parser.py
@@ -1,11 +1,12 @@
 #!/bin/python3
 
 #> Imports
-import click
 import typing
 from enum import IntEnum
 
 from . import operations
+
+from . import _eprint
 #</Imports
 
 #> Header >/
@@ -17,16 +18,16 @@ ExitCode = IntEnum('ExitCode', {'SUCCESS': 0,
 
 def split_operation(args: typing.Sequence[str]) -> tuple[typing.ForwardRef('operations.Operation') | ExitCode, typing.Sequence[str] | None]:
     if not (args and args[0].startswith('-') and (len(args[0]) > 1)): # catches: ``, `-`
-        click.echo('Error: missing operation', err=True)
+        _eprint('Error: missing operation', err=True)
         return (ExitCode.IMPROPER_USAGE, None)
     arg,*args = args
     if len(arg) > 2: # catches: `-Xx`, `--x`
         if arg[1] == '-': # catches: `--x`
             if (op := operations.op_from_long(arg[2:])) is not None: return (op, args)
-            click.echo(f'Error: unknown long-form operation {arg[2:]!r}', err=True)
+            _eprint(f'Error: unknown long-form operation {arg[2:]!r}', err=True)
             return (ExitCode.IMPROPER_USAGE, None)
         args.insert(0, f'-{arg[2:]}') # remove `x` from `-Xx` and add it to remaining args as `-x`
     # catches: `-X`
     if (op := operations.op_from_short(arg[1])) is not None: return (op, args)
-    click.echo(f'Error: unknown short-form operation {arg[1]!r}')
+    _eprint(f'Error: unknown short-form operation {arg[1]!r}')
     return (ExitCode.IMPROPER_USAGE, None)
