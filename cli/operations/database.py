4970a9b095f5c188dce815cc05606e1cb9c6c897
Shae <a.thomas.vc@gmail.com>
2024-03-17 17:27:03 -0700

    Brought database operation up-to-date with `fmlib` total autobinding

--- cli/operations/database.py
+++ cli/operations/database.py
@@ -3,83 +3,78 @@
 #> Imports
 import sys
 import types
-import typing
 import argparse
-from pathlib import Path
 from functools import partial
 
-from .. import common
-from ..parser import ExitCode
+from .. import preutil
+from .. import parsers
+from .. import postutil
 
-from .. import _eprint
+import FlexiLynx
 #</Imports
 
 #> Header >/
-__all__ = ('cli', 'parser',
-           'action_check', 'action_asdeps', 'action_asexplicit')
+__all__ = ('fill', 'main', 'actions')
 
-parser = argparse.ArgumentParser(prog=f'{sys.argv[0]} -D')
-common.root(parser)
-load_db = common.database(parser)
-exec_ep = common.entrypoint(parser, 2)
-menu = parser.add_mutually_exclusive_group(required=True)
-common.menu_arg(menu, 'action', 'k', 'check', help='Test database checksum')
-common.menu_arg(menu, 'action', None, 'asdeps', help='Mark packages as non-explicitly installed')
-common.menu_arg(menu, 'action', None, 'asexplicit', help='Mark packages as explicitly installed')
-missing = parser.add_mutually_exclusive_group(required=False)
-missing.add_argument('--ignore-missing', help='Don\'t fail if any targeted packages are missing', action='store_true')
-parser.add_argument('targets', nargs='*')
+def fill(ap: argparse.ArgumentParser):
+    menu = ap.add_mutually_exclusive_group(required=True)
+    menu = partial(preutil.menu_arg, menu, 'action')
+    menu('check', '-k', help='Test database checksum')
+    menu('asdeps', help='Mark packages as non-explicitly installed')
+    menu('asexplicit', help='Mark packages as explicitly installed')
+    ap.add_argument('--ignore-missing', help='Don\'t fail if any target packages are missing from the database', action='store_true')
+    ap.add_argument('targets', nargs='*', help='Package IDs to target')
+    # LGTM way to transfer the returned function from `postutil.handle_database()` to `main()`
+    ap.add_argument('--%dbgetter%', help=argparse.SUPPRESS, default=postutil.handle_database(ap, False),
+                    action=preutil.RaiseAction, const=TypeError(f'Why would you do this{"‽" if sys.getdefaultencoding == "utf-8" else "?!"}'))
+def main(ep: types.ModuleType, args: argparse.Namespace):
+    db = getattr(args, '%dbgetter%')(args)
+    print(1)
+    preutil.eprint('Obtaining database lock...')
+    try:
+        with db: actions[args.action](args, db)
+    finally:
+        preutil.eprint('Database lock successfully released')
 
-def cli(args: typing.Sequence[str]) -> ExitCode:
-    args = parser.parse_args(args)
-    ep,fmlib = exec_ep(args)
-    try: db = load_db(args, fmlib)
-    except common.UsageError as e:
-        _eprint(f'Error: {e.message}')
-        return ExitCode.IMPROPER_USAGE
-    _eprint('Obtaining database lock...')
-    with db:
-        _eprint(f'Dispatching to action_{args.action}()')
-        return globals()[f'action_{args.action}'](args, fmlib, ep.FlexiLynx, db)
-
-def action_check(args: argparse.Namespace, fmlib: types.ModuleType, fl: common.fmlib.FLType, db: common.fmlib.db.Controller) -> ExitCode:
+def _action_check(args: argparse.Namespace, db: postutil.fmlib.db.Controller):
     if args.targets:
-        _eprint('Error: extraneous arguments ("targets" should not be supplied with -k/--check)')
-        return ExitCode.IMPROPER_USAGE
-    _eprint('Reading database')
+        preutil.eprint('Error: extraneous arguments ("targets" should not be supplied with -k/--check)')
+        raise parsers.DoExit(parsers.ExitCode.USAGE | parsers.ErrorLocation.DATABASE)
+    preutil.eprint('Reading database')
     state = db.read()
-    _eprint('Generating checksum')
-    chksum = state.mkchksum(fl)
-    print(f'Reported:   {fl.core.util.base85.encode(state.chksum)}')
-    print(f'Calculated: {fl.core.util.base85.encode(chksum)}')
+    preutil.eprint('Generating checksum')
+    chksum = state.mkchksum()
+    print(f'Reported:   {FlexiLynx.core.util.base85.encode(state.chksum)}')
+    print(f'Calculated: {FlexiLynx.core.util.base85.encode(chksum)}')
     if state.chksum == chksum:
         print('Checksums match')
-        return ExitCode.SUCCESS
+        return
     print('Checksums do not match')
-    return ExitCode.GENERIC
+    raise parsers.DoExit(parsers.ExitCode.GENERIC | prasers.ErrorLocation.DATABASE)
 
-def _action_as_(expl: bool, args: argparse.Namespace, fmlib: types.ModuleType, fl: common.fmlib.FLType, db: common.fmlib.db.Controller) -> ExitCode:
+def _action_as_(expl: bool, args: argparse.Namespace, db: postutil.fmlib.db.Controller):
     if not args.targets:
-        _eprint('Nothing to do')
-        return ExitCode.SUCCESS
+        preutil.eprint('Nothing to do')
+        return
     targets = set(args.targets)
-    _eprint('Reading database')
+    preutil.eprint('Reading database')
     state = db.read()
     missing = targets - (state.expl.keys() | state.deps.keys())
     if missing:
-        _eprint(f'Some targets are not installed:\n{", ".join(missing)}')
+        preutil.eprint(f'Some targets are not installed:\n{", ".join(missing)}')
         if not args.ignore_missing:
-            _eprint('Error: some targets are not installed (pass --ignore-missing to ignore)')
-            return ExitCode.GENERIC
+            preutil.eprint('Error: some targets are not installed (pass --ignore-missing to ignore)')
+            raise parsers.DoExit(parsers.ExitCode.MISSING | parsers.ErrorLocation.DATABASE)
         targets -= missing
     targets -= (state.expl.keys() if expl else state.deps.keys())
     if not targets:
-        _eprint('Nothing to do')
-        return ExitCode.SUCCESS
+        preutil.eprint('Nothing to do')
+        return
     pop = (state.deps if expl else state.expl).pop
     (state.expl if expl else state.deps).update(zip(targets, map(pop, targets)))
-    _eprint('Writing database')
+    preutil.eprint('Writing database')
     db.write(state)
-    return ExitCode.SUCCESS
-action_asdeps = partial(_action_as_, False)
-action_asexplicit = partial(_action_as_, True)
+
+actions = {'check': _action_check,
+           'asdeps': partial(_action_as_, False),
+           'asexplicit': partial(_action_as_, True)}
